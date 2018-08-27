31b9be6c21c81ca8e68e62894f2eafa373dd7550
Danilo da Silva Pereira <dansilva41@hotmail.com>
2018-08-27 15:29:37 -0300

    Part. 04 - Stream para filtrar

---
+++ src/br/com/danilo/alura/teste/ExemploCursos.java
@@ -0,0 +1,28 @@
+package br.com.danilo.alura.teste;
+
+import br.com.danilo.alura.dominio.Curso;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+public class ExemploCursos {
+
+    public static void main(String[] args) {
+        List<Curso> cursos = new ArrayList<>();
+        cursos.add(new Curso("Python", 45));
+        cursos.add(new Curso("JavaScript", 150));
+        cursos.add(new Curso("Java 8", 113));
+        cursos.add(new Curso("C", 55));
+
+        // Ordenar pelo número de alunos
+        cursos.sort(Comparator.comparing(Curso::getAlunos));
+
+//        cursos.forEach(System.out::println);
+
+        // Filtrar por cursos com mais de 100 alunos
+
+        cursos.stream().filter(c -> c.getAlunos() >= 100).forEach(System.out::println);
+
+        }
+    }
